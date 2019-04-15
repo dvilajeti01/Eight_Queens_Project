87147c58db07912099495f3f098264d496ebf220
dvilajeti01 <dvilajeti01@manhattan.edu>
2019-04-14 20:21:08 -0400

    Add files via upload

--- Eight_Queens.cpp
+++ Eight_Queens.cpp
@@ -3,179 +3,200 @@
 //  AI_Project2
 //
 //  Created by Daniel Vilajeti on 4/10/19.
-//  Copyright Â© 2019 Daniel Vilajeti. All rights reserved.
+//  Copyright © 2019 Daniel Vilajeti. All rights reserved.
 //
 
 #include <iostream>
+#include <algorithm>
 #include "Eight_Queens.h"
 using namespace std;
 
 
-Eight_Queens :: Eight_Queens()
+Eight_Queens::Eight_Queens()
 {
-    //call rand array
+	//call rand array
 }
 
-Eight_Queens :: Eight_Queens(int *state)
+Eight_Queens::Eight_Queens(int state[8])
 {
-    memcpy(this->initial,state,sizeof(this->initial));
+	memcpy(this->initial, state, sizeof(this->initial));
 }
 
 Eight_Queens :: ~Eight_Queens()
 {
-    //??
+	//??
 }
 
-void Eight_Queens :: solve()
+void Eight_Queens::solve()
 {
-    printBoard(Hill_Climbing_Search(initial));
+	Hill_Climbing_Search(initial);
 }
 
-int * Eight_Queens:: Hill_Climbing_Search(int *initial_state)
+void Eight_Queens::Hill_Climbing_Search(int initial_state[8])
 {
-    //Do search here
-    int current[8];
-    int temp[8];
-    int min;
-    int attacks;
-    
-    memcpy(current,initial_state,sizeof(current)); //current = initial
-    
-    while (true)
-    {
-        min = 28; //Max amount of a attacks for any board config.
-        for (int i = 0; i < 8; i++) // 56 children
-        {
-            memcpy(temp,current,sizeof(temp));
-            attacks = generateChildren(temp, i);
-            if(attacks < min)
-            {
-                min = attacks;
-                memcpy(next,temp,sizeof(next));
-            }
-            
-        }
-        if (min >= numAttacks(current))
-            return current; // current is the peak
-        else
-            memcpy(current,next,sizeof(current));
-    }
+	//Do search here
+	int current[8];
+	int temp[8];
+	int min;
+	int attacks;
+
+	min = 28; //Max amount of a attacks for any board config.
+	copyArray(current, initial_state, 8);
+
+	while (true)
+	{
+
+		for (int i = 0; i < 8; i++) // 56 children
+		{
+			copyArray(temp, current, 8);
+
+			attacks = generateChildren(temp, i);
+
+			if (attacks < min)
+			{
+				min = attacks;
+				copyArray(next, temp, 8);
+			}
+
+		}
+		if (min >= numAttacks(current))
+		{
+
+			printBoard(current);
+			return; // current is the peak
+		}
+		else
+		{
+			copyArray(current, next, 8);
+		}
+	}
 }
-int Eight_Queens:: numAttacks(int *current)
+int Eight_Queens::numAttacks(int current[8])
 {
-    int totalAttacks = 0;
-    
-    for(int i = 0; i < 8; i++)
-    {
-        totalAttacks += (H_Attacks(current, i) + LU_Attacks(current, i) + LD_Attacks(current, i));
-    }
-    
-    return totalAttacks;
+	int totalAttacks = 0;
+
+	for (int i = 0; i < 8; i++)
+	{
+		totalAttacks += (H_Attacks(current, i) + LU_Attacks(current, i) + LD_Attacks(current, i));
+	}
+
+	return totalAttacks;
 }
 
-int Eight_Queens:: H_Attacks(int *current, int i)
+int Eight_Queens::H_Attacks(int current[8], int i)
 {
-    //calculate number attacks for given state
-    int numAttacks = 0;
-    
-    //horizontal attacks
-    for(int j = i+1; j < 8;j++)
-    {
-        if(current[i] == current[j])
-            numAttacks++;
-    }
+	//calculate number attacks for given state
+	int numAttacks = 0;
 
-    
-    return numAttacks;
+	//horizontal attacks
+	for (int j = i + 1; j < 8; j++)
+	{
+		if (current[i] == current[j])
+			numAttacks++;
+	}
+
+
+	return numAttacks;
 }
 
-int Eight_Queens:: LU_Attacks(int *current, int i)
+int Eight_Queens::LU_Attacks(int current[8], int i)
 {
-    //calculate number attacks for given state
-    int numAttacks = 0;
-    int row = 0;
-    int col = 0;
-   
-        row = current[i];
-        col = i;
-        
-        while(row >= 0 && col <= 7)
-        {
-            if(current[++col] == --row)
-            {
-                numAttacks++;
-            }
-        }
-    
-    return numAttacks;
+	//calculate number attacks for given state
+	int numAttacks = 0;
+	int row = 0;
+	int col = 0;
+
+	row = current[i];
+	col = i;
+
+	while (row >= 0 && col <= 7)
+	{
+		if (current[++col] == --row)
+		{
+			numAttacks++;
+		}
+	}
+
+	return numAttacks;
 }
 
-int Eight_Queens:: LD_Attacks(int *current, int i)
+int Eight_Queens::LD_Attacks(int current[8], int i)
 {
-    //calculate number attacks for given state
-    int numAttacks = 0;
-    int row = 0;
-    int col = 0;
-    
-    row = current[i];
-    col = i;
-    
-    while(row <= 7 && col <= 7)
-    {
-        if(current[++col] == ++row)
-        {
-            numAttacks++;
-        }
-    }
-    
-    return numAttacks;
+	//calculate number attacks for given state
+	int numAttacks = 0;
+	int row = 0;
+	int col = 0;
+
+	row = current[i];
+	col = i;
+
+	while (row <= 7 && col <= 7)
+	{
+		if (current[++col] == ++row)
+		{
+			numAttacks++;
+		}
+	}
+
+	return numAttacks;
 }
 
-int Eight_Queens:: generateChildren(int *current, int col)
+int Eight_Queens::generateChildren(int current[8], int col)
 {
-   //DETERMINE THE BEST CHILD FOR ALL MOVEMENTS OF A GIVEN QUEEN
-    int ignoreChange = current[col];
-    int next[8];
-    int attacks;
-    int min = numAttacks(current);
-    
-    for(int i = 0; i < 8; i++)
-    {
-        if(i != ignoreChange)
-        {
-            current[col] = i;
-            attacks = numAttacks(current);
-            if(attacks < min)
-            {
-                min = attacks;
-                memcpy(next,current,sizeof(next));
-            }
-        }
-    }
-    memcpy(current,next,sizeof(*current));
-    
-    return numAttacks(next);
+	//DETERMINE THE BEST CHILD FOR ALL MOVEMENTS OF A GIVEN QUEEN
+	int ignoreChange = current[col];
+	int best[8];
+	int attacks;
+	int min = numAttacks(current);
+	
+	copyArray(best, current, 8);
+
+	for (int i = 0; i < 8; i++)
+	{
+		if (i != ignoreChange)
+		{
+			current[col] = i;
+
+			attacks = numAttacks(current);
+
+			if (attacks < min)
+			{
+				min = attacks;
+				copyArray(best, current, 8);
+			}
+		}
+	}
+
+	copyArray(current, best, 8);
+	return min;
 }
 
-void Eight_Queens:: printBoard(int *state)
+void Eight_Queens::printBoard(int state[8])
 {
-   
-    for(int i = 0; i < 8; i++)
-    {
-        cout << "---------------------------------"<<endl;
-        
-        for(int j = 0; j < 8; j++)
-        {
-            if(state[j] == i)
-            {
-                cout<<"| Q ";
-            }
-            else
-                cout<<"|   ";
-        }
-        cout <<"|" << endl;
-    }
-    
-    cout << "---------------------------------"<<endl;
+
+	for (int i = 0; i < 8; i++)
+	{
+		cout << "---------------------------------" << endl;
+
+		for (int j = 0; j < 8; j++)
+		{
+			if (state[j] == i)
+			{
+				cout << "| Q ";
+			}
+			else
+				cout << "|   ";
+		}
+		cout << "|" << endl;
+	}
+
+	cout << "---------------------------------" << endl;
 }
 
+void Eight_Queens::copyArray(int to_array[8], int from_array[8], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		to_array[i] = from_array[i];
+	}
+}
--- Eight_Queens.h
+++ Eight_Queens.h
@@ -1,34 +1,37 @@
+#pragma once
+
 //
 //  Eight_Queens.h
 //  AI_Project2
 //
 //  Created by Daniel Vilajeti on 4/10/19.
-//  Copyright Â© 2019 Daniel Vilajeti. All rights reserved.
+//  Copyright © 2019 Daniel Vilajeti. All rights reserved.
 //
 
 class Eight_Queens
 {
 private:
-    
-    //current chess board configuration
-    int initial[8];
-    
-    //child of current chess board configuration
-    int next[8];
-    
-    
+
+	//current chess board configuration
+	int initial[8];
+
+	//child of current chess board configuration
+	int next[8];
+
+
 public:
-    
-    Eight_Queens();
-    Eight_Queens(int *state);
-    ~Eight_Queens();
-    void solve();
-    int * Hill_Climbing_Search(int *initial_state);
-    int numAttacks(int *current); //COMPLETE
-    int H_Attacks(int *current, int i); //COMPLETE
-    int LU_Attacks(int *current, int i); //COMPLETE
-    int LD_Attacks(int *current, int i); //COMPLETE
-    int generateChildren(int *current, int col);
-    void printBoard(int *state); //COMPLETE
-    
+
+	Eight_Queens();
+	Eight_Queens(int state[8]);
+	~Eight_Queens();
+	void solve();
+	void Hill_Climbing_Search(int initial_state[8]);
+	int numAttacks(int current[8]); //COMPLETE
+	int H_Attacks(int current[8], int i); //COMPLETE
+	int LU_Attacks(int current[8], int i); //COMPLETE
+	int LD_Attacks(int current[8], int i); //COMPLETE
+	int generateChildren(int current[8], int col);
+	void printBoard(int state[8]); //COMPLETE
+	void copyArray(int to_array[8], int from_array[8], int size);
+
 };
